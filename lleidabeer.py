d763939b07e91d076c06c03d308fb7111dcb3669
Carles Mateu <carlesm@carlesm.com>
2019-05-17 16:14:36 +0200

    Added click variant

---
+++ lleidabeer.py
@@ -0,0 +1,14 @@
+import click
+
+
+@click.command()
+@click.option("--debug",default=1,help="Debug level")
+@click.argument("inputfile", type=click.File('rb') )
+def control(debug,inputfile):
+    print("Control")
+    print(str(debug))
+
+
+
+if __name__ == "__main__":
+    control()
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+"""Brewery control.
 
+Usage:
+  main.py status <sensor> [--config=<cf>]
+  main.py reseet <sensor> [--config=<cf>]
+  main.py (-h | --help)
+  main.py --version
 
+Options:
+-h --help    Shows this screen
+--version    Shows program version
+
+"""
+from docopt import docopt
 
 
 if __name__ == "__main__":
-    print("Brewery")
+    arguments = docopt(__doc__,version="First version Brewery")
+    print(arguments)
